67eee2749b0b969ac07c3d38273e7dea4d9a560a
tvm-anija <anija.g@thrithvam.me>
2016-10-27 10:16:47 +0530

    update properties and methods for Priest

--- ChurchApp/DAL/Priest.cs
+++ ChurchApp/DAL/Priest.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Church.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,248 @@ namespace ChurchApp.DAL
 {
     public class Priest
     {
+        #region Public Properties
+        public string priestID
+        {
+            get;
+            set;
+        }
+        public string priestName
+        {
+            get;
+            set;
+        }
+        public string dob
+        {
+            get;
+            set;
+        }
+        public string about
+        {
+            get;
+            set;
+        }
+        public string dateOrdination
+        {
+            get;
+            set;
+        }
+        public string designation
+        {
+            get;
+            set;
+        }
+        public string address
+        {
+            get;
+            set;
+        }
+        public string emailId
+        {
+            get;
+            set;
+        }
+        public string mobile
+        {
+            get;
+            set;
+        }
+        public string imageId
+        {
+            get;
+            set;
+        }
+        public string churchID
+        {
+            get;
+            set;
+        }
+        public string createdBy
+        {
+            get;
+            set;
+        }
+        public DateTime createdDate
+        {
+            get;
+            set;
+        }
+        public string updatedBy
+        {
+            get;
+            set;
+        }
+        public DateTime updatedDate
+        {
+            get;
+            set;
+        }
+        #endregion Public Properties
+
+        #region Methods
+
+        #region SelectPriest
+        public DataSet SelectPriest()
+        {
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            DataSet ds = null;
+            SqlDataAdapter sda = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[GetAllPriests]";
+                cmd.Parameters.Add("@ChurchID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(churchID);
+                sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+                ds = new DataSet();
+                sda.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+            return ds;
+        }
+        #endregion SelectPriest
+
+        #region InsertPriest
+        public string InsertPriest()
+        {
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            SqlParameter outParam = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[InsertPriest]";
+                cmd.Parameters.Add("@Name", SqlDbType.NVarChar,150).Value = priestName;
+                cmd.Parameters.Add("@DOB", SqlDbType.Date).Value = Convert.ToDateTime(dob);
+                cmd.Parameters.Add("@About", SqlDbType.NVarChar, -1).Value = about;
+                cmd.Parameters.Add("@DateOrdination", SqlDbType.Date).Value = Convert.ToDateTime(dateOrdination);
+                cmd.Parameters.Add("@Designation", SqlDbType.NVarChar, 150).Value = designation;
+                cmd.Parameters.Add("@Address", SqlDbType.NVarChar, -1).Value = address;
+                cmd.Parameters.Add("@Email", SqlDbType.NVarChar,255).Value = emailId;
+                cmd.Parameters.Add("@Mobile", SqlDbType.NVarChar, 20).Value = mobile;
+                cmd.Parameters.Add("@ChurchID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(churchID);
+                cmd.Parameters.Add("@ImageID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(imageId);
+                cmd.Parameters.Add("@CreatedBy", SqlDbType.NVarChar, 100).Value = createdBy;
+                cmd.Parameters.Add("@CreatedDate", SqlDbType.DateTime).Value = DateTime.Now;
+                outParam = cmd.Parameters.Add("@InsertStatus", SqlDbType.TinyInt);
+                outParam.Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+            return outParam.Value.ToString();
+        }
+        #endregion InsertPriest
+
+        #region UpdatePriest
+        public string UpdatePriest()
+        {
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            SqlParameter outParam = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[UpdatePriest]";
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(priestID);
+                cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 150).Value = priestName;
+                cmd.Parameters.Add("@DOB", SqlDbType.Date).Value = Convert.ToDateTime(dob);
+                cmd.Parameters.Add("@About", SqlDbType.NVarChar, -1).Value = about;
+                cmd.Parameters.Add("@DateOrdination", SqlDbType.Date).Value = Convert.ToDateTime(dateOrdination);
+                cmd.Parameters.Add("@Designation", SqlDbType.NVarChar, 150).Value = designation;
+                cmd.Parameters.Add("@Address", SqlDbType.NVarChar, -1).Value = address;
+                cmd.Parameters.Add("@Email", SqlDbType.NVarChar, 255).Value = emailId;
+                cmd.Parameters.Add("@Mobile", SqlDbType.NVarChar, 20).Value = mobile;
+                cmd.Parameters.Add("@ChurchID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(churchID);
+                cmd.Parameters.Add("@ImageID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(imageId);
+                cmd.Parameters.Add("@UpdatedBy", SqlDbType.NVarChar, 100).Value = updatedBy;
+                cmd.Parameters.Add("@UpdateStatus", SqlDbType.DateTime).Value = DateTime.Now;
+                outParam = cmd.Parameters.Add("@UpdateStatus", SqlDbType.TinyInt);
+                outParam.Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+            return outParam.Value.ToString();
+        }
+        #endregion UpdatePriest
+
+        #region DeletePriest
+        public string DeletePriest()
+        {
+            dbConnection dcon = null;
+            SqlCommand cmd = null;
+            SqlParameter outParam = null;
+            try
+            {
+                dcon = new dbConnection();
+                dcon.GetDBConnection();
+                cmd = new SqlCommand();
+                cmd.Connection = dcon.SQLCon;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "[DeletePriest]";
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(priestID);
+                cmd.Parameters.Add("@ChurchID", SqlDbType.UniqueIdentifier).Value = Guid.Parse(churchID);
+                outParam = cmd.Parameters.Add("@DeleteStatus", SqlDbType.TinyInt);
+                outParam.Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dcon.SQLCon != null)
+                {
+                    dcon.DisconectDB();
+                }
+            }
+            return outParam.Value.ToString();
+        }
+        #endregion DeletePriest
+
+        #endregion Methods
     }
 }
