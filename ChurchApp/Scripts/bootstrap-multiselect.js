f3b1a7e698e0a547e7bbdc6960e74651f46128c5
tvm-anija <anija.g@thrithvam.me>
2016-12-14 13:41:53 +0530

    updated mass schedules

--- ChurchApp/Scripts/bootstrap-multiselect.js
+++ ChurchApp/Scripts/bootstrap-multiselect.js
@@ -423,7 +423,7 @@
             disabledText: '',
             delimiterText: ', ',
             templates: {
-                button: '<input type="text" class="" placeholder="Select Days" data-toggle="dropdown" disabled></input>',
+                button: '<input type="text" class="" placeholder="Select Days" data-toggle="dropdown" readonly></input>',
                 ul: '<ul class="multiselect-container dropdown-menu"></ul>',
                 filter: '<li class="multiselect-item multiselect-filter"><div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span><input class="form-control multiselect-search" type="text"></div></li>',
                 filterClearBtn: '<span class="input-group-btn"><button class="btn btn-default multiselect-clear-filter" type="button"><i class="glyphicon glyphicon-remove-circle"></i></button></span>',
