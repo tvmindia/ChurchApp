9fe79d483b4aaa6673bd6088cb0b56724e0b851d
tvm-anija <anija.g@thrithvam.me>
2017-01-05 17:09:00 +0530

    removed unwanted using from gallery and families

--- ChurchApp/AdminPanel/Gallery.aspx.cs
+++ ChurchApp/AdminPanel/Gallery.aspx.cs
@@ -2,12 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.Services;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ChurchApp.AdminPanel
 {
