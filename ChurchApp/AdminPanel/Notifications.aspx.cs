8214f256d376ed7efa0f99fba911927715ff29d2
tvm-anija <anija.g@thrithvam.me>
2017-01-05 17:11:53 +0530

    removed unwanted usings

--- ChurchApp/AdminPanel/Notifications.aspx.cs
+++ ChurchApp/AdminPanel/Notifications.aspx.cs
@@ -8,12 +8,9 @@ using ChurchApp.DAL;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.Services;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
 #endregion NameSpace
 
 namespace ChurchApp.AdminPanel
--- ChurchApp/AdminPanel/Priests.aspx.cs
+++ ChurchApp/AdminPanel/Priests.aspx.cs
@@ -7,13 +7,10 @@ using ChurchApp.DAL;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.Services;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
 #endregion Namespace
 #region MainClass
 namespace ChurchApp.AdminPanel
