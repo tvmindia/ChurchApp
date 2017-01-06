1d9c7d9e3fad9694e5f9b06577530ebaaf264d4d
tvm-gibin <gibin.jacob@thrithvam.me>
2017-01-06 14:48:09 +0530

    Include methods for excel import

--- ChurchApp/DAL/ImportExcel.cs
+++ ChurchApp/DAL/ImportExcel.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OleDb;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -17,11 +19,202 @@ namespace ChurchApp.DAL
         {
             get;
             set;
+        }     
+
+        public string fileName
+        {
+            get;
+            set;
+        }
+        public string ExcelConnectionString
+        {
+            get;
+            set;
+        }
+        public string fileLocation
+        {
+            get;
+            set;
+        }
+        public string SheetDescription
+        {
+            get;
+            set;
+        }
+        public string SheetName
+        {
+            get;
+            set;
+        }
+
+        public string temporaryFolder { get; set; }
+        public HttpRequestBase request
+        {
+            get;
+            set;
+        }
+        public string ExcelFileName
+        {
+            get;
+            set;
         }
 
         #endregion Public Properties
 
         #region Methods
+
+        #region ExcelSheetNames
+
+        public static string FileDescriptionSheetName
+        {
+            get { return "Field Description"; }
+        }
+        public static string Church
+        {
+            get { return "Church"; }
+        }
+
+        #endregion ExcelSheetNames
+
+
+        #region GetExcelData
+        public DataSet GetExcelData(DataSet dsTable)
+        {
+            var Request = request;
+            string tempFolder = temporaryFolder;
+            DataSet dsFile = new DataSet();
+            String[] excelSheets;
+            try
+            {
+                if (ExcelFileName.Length > 0)
+                {
+                    
+                    excelSheets = OpenExcelFile();
+                    dsFile = ScanExcelFileToDS(excelSheets, dsTable);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+            }
+
+            return dsFile;
+        }
+        #endregion GetExcelData
+
+        #region ScanExcelFileToDS
+        public DataSet ScanExcelFileToDS(string[] excelSheets, DataSet dsTable)
+        {
+
+            DataSet dsFile = new DataSet();
+            OleDbConnection excelConnection1 = new OleDbConnection(ExcelConnectionString);
+            try
+            {
+                excelConnection1.Open();
+                var command = excelConnection1.CreateCommand();
+
+             
+                command.CommandText = string.Format("Select * from [{0}]", SheetName + "$");
+                var conditions = "";
+                foreach (DataRow dr in dsTable.Tables[0].Rows)
+                {
+                    conditions += "[" + dr["Field_Description"].ToString() + "]" + " IS NOT NULL OR ";
+                }
+                if (conditions != "")
+                {
+                    command.CommandText += " WHERE " + conditions.Remove(conditions.Length - 4);
+                }
+
+            
+                using (OleDbDataAdapter dataAdapter = new OleDbDataAdapter(command.CommandText, excelConnection1))
+                {
+                    dataAdapter.Fill(dsFile);
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                excelConnection1.Close();
+            }
+
+            return dsFile;
+        }
+
+        #endregion ScanExcelFileToDS
+
+
+        #region OpenExcelFile
+        public string[] OpenExcelFile()
+        {
+            string fileExtension = Path.GetExtension(fileName);
+            ExcelConnectionString = string.Empty;
+            if (fileExtension == ".xls")
+            {
+                ExcelConnectionString = System.Configuration.ConfigurationManager.AppSettings["XLS_ConnectionString"];
+                ExcelConnectionString = ExcelConnectionString.Replace("$fileLocation$", fileLocation);
+            }
+            //connection String for xlsx file format.
+            else if (fileExtension == ".xlsx")
+            {
+                ExcelConnectionString = System.Configuration.ConfigurationManager.AppSettings["XLSX_ConnectionString"];
+                ExcelConnectionString = ExcelConnectionString.Replace("$fileLocation$", fileLocation);
+            }
+            //Create Connection to Excel work book and add oledb namespace
+            OleDbConnection excelConnection = new OleDbConnection(ExcelConnectionString);
+            String[] excelSheets = null;
+            try
+            {
+                if (excelConnection.State != ConnectionState.Open)
+                {
+                    excelConnection.Open();
+                }
+                DataTable dt = new DataTable();
+
+                dt = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                if ((dt.Rows.Count == 2) && (dt != null))
+                {
+                    excelSheets = new String[dt.Rows.Count];
+                    int t = 0;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        if ("'" + FileDescriptionSheetName + "$" + "'" == row["TABLE_NAME"].ToString())
+                        {
+                            SheetDescription = row["TABLE_NAME"].ToString();
+                            SheetDescription = SheetDescription.TrimEnd('$');
+                        }
+                        else
+                        {
+                            SheetName = row["TABLE_NAME"].ToString();
+                            SheetName = SheetName.TrimEnd('$');
+                        }
+                        excelSheets[t] = row["TABLE_NAME"].ToString();
+                        t++;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                excelConnection.Close();
+            }
+            return excelSheets;
+        }
+        #endregion OpenExcelFile
+       
+
+    
 
         #region GetTableDefinition
         /// <summary>
@@ -65,6 +258,7 @@ namespace ChurchApp.DAL
 
         #endregion Methods
 
+
         public bool Validate()
         {
             bool status = true;
@@ -72,5 +266,7 @@ namespace ChurchApp.DAL
             return status;
         }
 
+
+       
     }
 }
