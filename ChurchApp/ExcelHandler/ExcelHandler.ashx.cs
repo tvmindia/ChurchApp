000f48943deadcf2b4989f3847ea2be424c22da5
tvm-gibin <gibin.jacob@thrithvam.me>
2017-01-06 14:17:17 +0530

    new file added Excel Handler

---
+++ ChurchApp/ExcelHandler/ExcelHandler.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChurchApp.ExcelHandler
+{
+    /// <summary>
+    /// Summary description for ExcelHandler
+    /// </summary>
+    public class ExcelHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
