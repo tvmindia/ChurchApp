1430fd0604310c4e22bb4bc2ba14ad79b406ccad
tvm-gibin <gibin.jacob@thrithvam.me>
2017-01-07 13:27:29 +0530

    Import Excel to DataSet

--- ChurchApp/AdminPanel/ImportExcel.aspx.cs
+++ ChurchApp/AdminPanel/ImportExcel.aspx.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ChurchApp.DAL;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Data;
 
 namespace ChurchApp.AdminPanel
 {
@@ -13,5 +17,53 @@ namespace ChurchApp.AdminPanel
         {
 
         }
+        //
+
+        //<--------------------- Select Excel TableNames----------------------------->//
+        #region SelectExcelTableNames
+        [WebMethod(EnableSession = true)]
+        public static string SelectExcelTableNames(DAL.ImportExcel impxlObj)
+        {
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Security.UserAuthendication UA = null;
+            try
+            {
+                DashBoard dashBoardObj = new DashBoard();
+                UA = dashBoardObj.GetCurrentUserSession();
+                if (UA != null)
+                {
+                    DataSet ds = null;
+
+                    ds = impxlObj.SelectTableNames();
+                    Dictionary<string, object> childRow;
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        foreach (DataRow row in ds.Tables[0].Rows)
+                        {
+                            childRow = new Dictionary<string, object>();
+                            foreach (DataColumn col in ds.Tables[0].Columns)
+                            {
+                                childRow.Add(col.ColumnName, row[col]);
+                            }
+                            parentRow.Add(childRow);
+                        }
+
+                    }
+                }
+                //Session is out
+                else
+                {
+                    Common comonObj = new Common();
+                    return jsSerializer.Serialize(comonObj.RedirctCurrentRequest());
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return jsSerializer.Serialize(parentRow);
+        }
+        #endregion SelectExcelTableNames
     }
 }
--- ChurchApp/ExcelHandler/ExcelHandler.ashx.cs
+++ ChurchApp/ExcelHandler/ExcelHandler.ashx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Configuration; 
 
 namespace ChurchApp.ExcelHandler
 {
@@ -14,12 +16,110 @@ namespace ChurchApp.ExcelHandler
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
+            DAL.Security.UserAuthendication UA = new DAL.Security.UserAuthendication();
+            DAL.ImportExcel ImportXL = new DAL.ImportExcel();
+            string currentSheet = null;  
+            DataSet dsFile = null;
+            DataSet dsTable = null;
+            HttpPostedFile postFile = null;
+
+            try
+            {
+
+                if (context.Request.Files.Count > 0)//chechhasfile DataImportFileUpload
+                {
+                    if (context.Request.Form.GetValues("ActionTyp") != null)
+                    {
+                        switch (context.Request.Form.GetValues("ActionTyp")[0])
+                        {
+                            case "Church":
+
+                                String[] excelSheets = null;
+                                string path = HttpContext.Current.Server.MapPath(ConfigurationManager.ConnectionStrings["TempFilePath"].ConnectionString).ToString();
+
+                                postFile = context.Request.Files["upImageFile"];
+                                string fileName = UA.ChurchID + "_" + postFile.FileName.ToString();
+
+
+                                string fileLocation = path + fileName;
+                                string fileExtension = System.IO.Path.GetExtension(fileName);
+
+                                ImportXL.fileName = fileName;
+                                ImportXL.fileLocation = fileLocation;
+                                ImportXL.tableName = "Church";
+
+                                DeleteDuplicateFile(fileLocation);//deletes the file if the same file name exists in the folder
+                                postFile.SaveAs(fileLocation);
+                                excelSheets = ImportXL.OpenExcelFile();
+                                if (excelSheets != null)
+                                {                                   
+                                        dsFile = new DataSet();
+                                        dsTable = ImportXL.GetTableDefinition();
+                                        dsFile = ImportXL.ScanExcelFileToDS(excelSheets, dsTable);
+                                        //columnExistCheck = validationObj.ValidateExcelDataStructure(dsFile, dsTable);
+                                        //if (columnExistCheck == true)
+                                        //{
+
+                                        //}
+                                        //if (columnExistCheck == false)
+                                        //{
+
+                                        //}
+                                  
+                                }
+                                else
+                                {
+
+                                }
+                                break;
+                            default:
+                                break;
+
+
+
+                        }//end of hasfile if
+                    }
+                }
+               // ScriptManager.RegisterStartupScript(this, this.GetType(), "Upload", "GenerateTemplateNextClick();", true);
+
+            }//end try
+            catch (Exception ex)
+            {
+              
+            }
+            finally
+            {
+
+            }       
+
             context.Response.Write("Hello World");
         }
 
         public bool IsReusable
         {
             get
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteDuplicateFile(string location)
+        {
+            if (System.IO.File.Exists(location))
+            {
+                try
+                {
+                    System.IO.File.Delete(location);
+                    return true;
+                }
+
+                catch (Exception ex)
+                {                  
+                
+                    throw ex;
+                }
+            }
+            else
             {
                 return false;
             }
