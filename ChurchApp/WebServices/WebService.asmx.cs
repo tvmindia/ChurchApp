25b4007a46ec7b5e62863a006b3fd29502c60be7
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-01-18 16:20:57 +0530

    Gallery albums with both images and videos

--- ChurchApp/WebServices/WebService.asmx.cs
+++ ChurchApp/WebServices/WebService.asmx.cs
@@ -680,22 +680,29 @@ namespace ChurchApp.WebServices
                 {
                     if (dt.Rows[i]["AlbumType"].ToString() == "video")  //Have to make thumbnail
                     {
-                        if (dt.Rows[i]["URL"].ToString().Contains("youtube"))//Get youtube thumbnail
+                        try
                         {
-                            var youtube_video_id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("embed/") + 6);
-                            dt.Rows[i]["URL"] = "http://img.youtube.com/vi/" + youtube_video_id + "/0.jpg";
+                            if (dt.Rows[i]["URL"].ToString().Contains("youtube"))//Get youtube thumbnail
+                            {
+                                var youtube_video_id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("embed/") + 6);
+                                dt.Rows[i]["URL"] = "http://img.youtube.com/vi/" + youtube_video_id + "/0.jpg";
+                            }
+                            else if (dt.Rows[i]["URL"].ToString().Contains("vimeo"))//Get vimeo thumbnail
+                            {
+                                var vimeo_video_id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("video/") + 6);
+                                dt.Rows[i]["URL"] = "http://i.vimeocdn.com/video/" + vimeo_video_id + ".jpg";
+                            }
+                            else    //video is from own server
+                            {
+                                string id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("vid/") + 4);
+                                id = id.Substring(0, id.LastIndexOf("."));
+                                dt.Rows[i]["URL"] = "/vid/Poster/" + id + ".jpg";
+                            }
                         }
-                        else if (dt.Rows[i]["URL"].ToString().Contains("vimeo"))//Get vimeo thumbnail
-                        {
-                            var vimeo_video_id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("video/") + 6);
-                            dt.Rows[i]["URL"] = "http://i.vimeocdn.com/video/" + vimeo_video_id + ".jpg";
+                        catch(Exception ex){
+                            dt.Rows[i]["URL"] = "null";
                         }
-                        else    //video is from own server
-                        {
-                            string id = dt.Rows[i]["URL"].ToString().Substring(dt.Rows[i]["URL"].ToString().LastIndexOf("vid/") + 4);
-                            id=id.Substring(0, id.LastIndexOf("."));
-                            dt.Rows[i]["URL"] = "/vid/Poster/"+id+".jpg";
-                        }
+                      
                     }
                     
 
