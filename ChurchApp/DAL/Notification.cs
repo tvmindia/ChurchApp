35de4bad39ecc92b6fbe9bb1acce39bdfdc4520c
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-01-25 11:11:25 +0530

    notification cs changes

--- ChurchApp/DAL/Notification.cs
+++ ChurchApp/DAL/Notification.cs
@@ -480,10 +480,10 @@ namespace ChurchApp.DAL
                         {
                             to =to_String,
 
-                            notification = new
+                            data = new
                             {
-                                title= titleString,
-                                body= descriptionString,
+                                title = titleString,
+                                body = descriptionString,
                                 sound = "default"
                             }
                         };
