b23155d6440f5df12f123404ed9d644a7d1dc316
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-02-16 17:54:56 +0530

    notification service

--- ChurchApp/Services/NotificationSendingService.cs
+++ ChurchApp/Services/NotificationSendingService.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Web;
+using System.Data;
+using ChurchApp.DAL;
 
 namespace ChurchApp.Services
 {
@@ -27,18 +26,24 @@ namespace ChurchApp.Services
 
             while (true)
             {
-                var minutes = 2;
-                System.Threading.Thread.Sleep(minutes * 60 * 1000);
+                var hours = 24;
+                System.Threading.Thread.Sleep(hours * 60 * 60 * 1000);
                 try
                 {
 
-                    //------------------Deleting temporary files created by excel files----------------
-                    string[] filePaths2 = Directory.GetFiles(HttpContext.Current.Server.MapPath("~/TempFiles/"));
-                    foreach (string filePath in filePaths2)
-                        if (DateTime.UtcNow - File.GetCreationTimeUtc(filePath) > TimeSpan.FromHours(10))
-                        {
-                            File.Delete(filePath);
-                        }
+                    //------------------Sending notifications which are scheduled today----------------
+                    DataTable dt = new DataTable();
+                    NotificationSchedule noti = new NotificationSchedule();
+                    dt = noti.SelectNotificationsOnScheduledDate().Tables[0];
+                    Notification notiTemp = new Notification();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+
+
+                        notiTemp.SendToFCM(dr["Caption"].ToString(), dr["Description"].ToString(), false, dr["ChurchID"].ToString());
+                     
+                    }                  
+                   
 
                 }
                 catch (Exception ex)
